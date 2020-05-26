7ed5e3602a862ff0e28f781c2716dcb79742a31a
Jorge <valencia12@github.com>
2020-05-25 21:11:13 -0600

    Add fun

--- src/main/java/com/uca/capas/JPAConfiguration.java
+++ src/main/java/com/uca/capas/JPAConfiguration.java
@@ -1,5 +1,7 @@
 package com.uca.capas;
 
+import javax.persistence.EntityManagerFactory;
+
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -8,10 +10,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class JPAConfiguration {
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
@@ -33,7 +38,7 @@ public class JPAConfiguration {
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/Parcial");
 		dataSource.setUsername("postgres");
-		dataSource.setPassword("sasuke");
+		dataSource.setPassword("password");
 		return dataSource;
 	}
 	
@@ -42,6 +47,14 @@ public class JPAConfiguration {
 		properties.setProperty("hibernate.show_sql", "true");
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 		return properties;
+	}
+
+	@Bean
+	JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
+
 	}
 	
 }
